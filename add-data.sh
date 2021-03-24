8f5c1560b6ffbebcf6f48dcf0dae323060d3997c
Artem <artarik@yandex.ru>
2021-03-24 14:11:25 +0000

    Change add-data.sh

--- add-data.sh
+++ add-data.sh
@@ -7,9 +7,8 @@ set -o nounset
 IFS=$(printf '\n\t')
 
 # Docker
-sudo apt remove --yes docker docker-engine docker.io containerd runc
 sudo apt update
-sudo apt --yes --no-install-recommends install apt-transport-https ca-certificates
+sudo apt --yes --no-install-recommends install apt-transport-https ca-certificates curl lsb-release
 wget --quiet --output-document=- https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=$(dpkg --print-architecture)] https://download.docker.com/linux/ubuntu $(lsb_release --codename --short) stable"
 sudo apt update
@@ -22,7 +21,10 @@ printf 'Waiting for Docker to start...\n\n'
 sleep 5
 
 # Docker Compose
-sudo wget --output-document=/usr/local/bin/docker-compose "https://github.com/docker/compose/releases/download/$(wget --quiet --output-document=- https://api.github.com/repos/docker/compose/releases/latest | grep --perl-regexp --only-matching '"tag_name": "\K.*?(?=")')/run.sh"
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.28.6/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+#sudo wget --output-document=/usr/local/bin/docker-compose "https://github.com/docker/compose/releases/download/$(wget --quiet --output-document=- https://api.github.com/repos/docker/compose/releases/latest | grep --perl-regexp --only-matching '"tag_name": "\K.*?(?=")')/run.sh"
 sudo chmod +x /usr/local/bin/docker-compose
 sudo wget --output-document=/etc/bash_completion.d/docker-compose "https://raw.githubusercontent.com/docker/compose/$(docker-compose version --short)/contrib/completion/bash/docker-compose"
 printf '\nDocker Compose installed successfully\n\n'
